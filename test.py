d0f57eff1de5c803c5a12487f968de4f33c3e140
Lonesome George <kingdeadfish@qq.com>
2014-12-30 12:25:54 +0800

    update

---
+++ test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: KIDJourney
+# @Date:   2014-12-27
+# @Last Modified by:   KIDJourney
+# @Last Modified time: 2014-12-27
+import urllib
+154926fbef1d97509de9d2dfb5b978f6
+　　#include
+　　int main( void )
+　　{　
+　　for(;{
+　　　　　　　printf( "hung up\t\t\b\b\b\b\b\b" );
+　　　　　　　print("hung up\t\t\b\b\b\b\b\b";
+　　　　　　}
+　　return 0;
+　　}
+#include<stdio.h>
+int main(void)
+{
+    while(1)
+    {
+        char *Ptr = NULL; 
+        Ptr = (char *)malloc(100 * sizeof(char)); 
+    }
+}
